6940ce6b11a63198b949989a12b9c1a03bb3fb88
Brandon Michael Taft <btaft6278@gmail.com>
2021-11-10 15:15:56 -0500

    Update HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -39,12 +39,12 @@ function HomePage() {
                 </div>
 
                 <div class="grid-container">
-                    <a class="grid-item" href="https://damp-spire-28696.herokuapp.com/quiz/9">
+                    <a class="grid-item" href="/quiz/9">
                         <FontAwesomeIcon icon={faBrain} />
                         <br></br>
                         General Info
                     </a>
-                    <a class="grid-item" href="https://damp-spire-28696.herokuapp.com/quiz/11">
+                    <a class="grid-item" href="/quiz/11">
                        <FontAwesomeIcon icon={faFilm} />
                         <br></br>
                         Movies
